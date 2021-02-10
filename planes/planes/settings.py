da3a7e286aa9f9f5afa1bc9f06d37ae1b0764c73
unrecano <unrecano@gmail.com>
2021-02-10 15:00:08 -0500

    incluir allowed hosts desde código.

--- planes/planes/settings.py
+++ planes/planes/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = [os.getenv('ALLOWED_HOSTS')]
-
+ALLOWED_HOSTS = ['.recano.club', '.recano.club.', 'localhost', '0.0.0.0']
 
 # Application definition
 
